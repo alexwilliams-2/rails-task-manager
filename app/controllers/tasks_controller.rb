2e9da6c5c826246489a1bf77ddab4e2fbccabae2
Alex Williams <ahnnwilliams@hotmail.co.uk>
2023-04-29 15:32:19 +0100

    added show, new task, edit functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,37 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  # Get the user form
+  def new
+    @task = Task.new
+  end
+
+  # Create new instance from form
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  # Strong paramenters
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Read home page / index
+  get "tasks", to: "tasks#index", as: :tasks
+
+  # Create new task - takes you to form
+  get "tasks/new", to: "tasks#new", as: :new_task
+
+  # Read individal task
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # Post task - submit form and save params to database
+  post "tasks", to: "tasks#create"
+
+  # Update task - step 1 - get the form
+  get "tasks/:id/edit", to: "tasks#edit"
+  # Update task - step 2 - update the task instance
+  patch "tasks/:id", to: "tasks#update"
 end
